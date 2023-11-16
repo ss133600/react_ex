a9cae7e24c9fd479432069479e877cd600af518c
ss133600 <S9878133600@gmail.com>
2023-11-17 04:48:57 +0900

    UseEffectEx, ExceptionEx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { ExceptionEx } from "./components/ExceptionEx";
+import { UseEffectEx } from "./components/UseEffectEx";
+
 const App = () => {
-  return <div>app</div>;
+  return (
+    <div>
+      {/* <UseEffectEx /> */}
+      <ExceptionEx />
+    </div>
+  );
 };
 
 export default App;
--- src/components/UseEffectEx.js
+++ src/components/UseEffectEx.js
@@ -3,31 +3,33 @@ import { useEffect, useState } from "react";
 export const UseEffectEx = () => {
   const [num, setNum] = useState(0);
 
+  //   console.log(num);
+
   //   useEffect(() => {
-  //     console.log("마운트와 업데이트 할 때 마다");
+  //     console.log("마운트와 업데이트 할 때 마다 실행함");
   //   });
 
   //   useEffect(() => {
-  //     console.log("마운트 될때만 실행");
+  //     console.log("마운트 될때 실행");
   //   }, []);
 
-  useEffect(() => {
-    console.log("num 값이 변경 될때만 실행");
-  }, [num]);
+  //   useEffect(() => {
+  //     console.log("num 값이 변경될때 만 실행");
+  //   }, [num]);
 
   return (
     <div>
       <h3>{num}</h3>
-      {num} <button onClick={() => setNum(num + 1)}>+</button>
+      <button onClick={() => setNum(num + 1)}>+</button>
       <button onClick={() => setNum(num - 1)}>-</button>
     </div>
   );
 };
 
 // *useEffect
-// =>앱이 렌더링, 리렌더링, 페이지 이동 등 이런 상황에서
-// 부수적인 동작을 할 때 사용
+// => 앱이 렌더링, 리렌더링, 페이지 이동 등 이런 상황에서 부수적인 동작을
+// 할 때 사용
 
 // mount: 첫 렌더링
 // update: 리 렌더링
-// unmount: 화면에서 사라질 때
+// unmount: 화면에서 사라질때
